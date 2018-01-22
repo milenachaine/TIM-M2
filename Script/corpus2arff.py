fb0291a27ec8cb273c7087e1c2234922f3212638
eldams <damien.nouvel@inalco.fr>
2018-01-22 12:08:08 +0100

    Attributs

--- Script/corpus2arff.py
+++ Script/corpus2arff.py
@@ -2,21 +2,34 @@
 
 import xml.etree.ElementTree as ET
 
-print('Initiating arff file')
-arfffile = open('../Corpus/all.arff', 'w')
-arfffile.write('@relation fakevstrusted\n')
-arfffile.write('@attribute text string\n')
-arfffile.write('@attribute class {fake,trusted}\n')
-arfffile.write('@data\n')
+print('Initiating arff files')
+header = '''@relation fakevstrusted
+@attribute monattribut numeric
+@attribute class {fake,trusted}
+@data
+'''
+arfffiletrain = open('../Corpus/train.arff', 'w')
+arfffiletrain.write(header)
+arfffiletest = open('../Corpus/test.arff', 'w')
+arfffiletest.write(header)
 
 print('Reading corpus to arff format')
 xmlcorpus = ET.parse('../Corpus/all.xml')
+nodoc = 0
 for doc in xmlcorpus.getroot():
+	# print(nodoc,nodoc%4)
 	text = doc.text
-	text = text.strip()
-	text = text.replace("\n", " ")
-	text = text.replace("'", "\\'")
-	arfffile.write("'"+text+"'")
-	arfffile.write(',')
-	arfffile.write(doc.get('class'))
-	arfffile.write('\n')
+	# text = text.strip()
+	# text = text.replace("\n", " ")
+	# text = text.replace("'", "\\'")
+	# arfffiletrain.write("'"+text+"'")
+	if 'dollar' in text:
+		monattribut = 1
+	else:
+		monattribut = 0
+	docattr = str(monattribut)+','+doc.get('class')+'\n'
+	if nodoc%2 == 0:
+		arfffiletest.write(docattr)
+	else:
+		arfffiletrain.write(docattr)
+	nodoc += 1
