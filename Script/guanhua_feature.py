c7543177309a89b1be50bdc4c25b98b933c042c2
Guanhua <guanhua_wang@yahoo.com>
2018-01-22 12:16:01 +0100

    modif

--- Script/guanhua_feature.py
+++ Script/guanhua_feature.py
@@ -2,24 +2,7 @@
 
 import xml.etree.ElementTree as ET
 
-print('Initiating arff file')
-arfffile = open('../Corpus/all.arff', 'w')
-arfffile.write('@relation fakevstrusted\n')
-arfffile.write('@attribute text string\n')
-arfffile.write('@attribute class {fake,trusted}\n')
-arfffile.write('@data\n')
-
-print('Reading corpus to arff format')
 xmlcorpus = ET.parse('../Corpus/all.xml')
-for doc in xmlcorpus.getroot():
-	text = doc.text
-	text = text.strip()
-	text = text.replace("\n", " ")
-	text = text.replace("'", "\\'")
-	arfffile.write("'"+text+"'")
-	arfffile.write(',')
-	arfffile.write(doc.get('class'))
-	arfffile.write('\n')
 
 test = open('../Corpus/test.arff', 'w')
 train = open('../Corpus/train.arff','w')
