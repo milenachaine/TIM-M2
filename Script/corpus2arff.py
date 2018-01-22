c1cf0447e13afc9a59a904c59dae2e650d7c931f
eldams <damien.nouvel@inalco.fr>
2018-01-22 15:18:52 +0100

    Classification weka train / test avec features

--- Script/corpus2arff.py
+++ Script/corpus2arff.py
@@ -2,34 +2,78 @@
 
 import xml.etree.ElementTree as ET
 
-print('Initiating arff files')
-header = '''@relation fakevstrusted
-@attribute monattribut numeric
-@attribute class {fake,trusted}
-@data
-'''
+features = {
+	# sandra
+	# abdenour
+	# agathe
+	# andrea
+	# arthur
+	# audrey
+	# chloe
+	# damien
+	'ameri': 'numeric',
+	'impers': 'numeric',
+	# giovanna
+	# jielei
+	# mingqiang
+	# morgane
+	# nico
+	# xi
+	# yousef
+	# yunbei
+	# yuran
+}
+
+def getfeature(text, name):
+	# sandra
+	# abdenour
+	# agathe
+	# andrea
+	# arthur
+	# audrey
+	# chloe
+	# damien
+	if name == 'ameri':
+		if 'Améri' in text:
+			return 1
+		return 0
+	if name == 'impers':
+		nbimpers = 0
+		nbimpers += text.count('on')
+		nbimpers += text.count('nous')
+		return nbimpers
+	# giovanna
+	# jielei
+	# mingqiang
+	# morgane
+	# nico
+	# xi
+	# yousef
+	# yunbei
+	# yuran
+	return None
+
+print('Initialize arff files')
+header = ''
+header += '@relation fakevstrusted\n'
+for f in features:
+	header += '@attribute '+f+' '+features[f]+'\n'
+header += '@attribute class {fake,trusted}\n'
+header += '@data\n'
+
 arfffiletrain = open('../Corpus/train.arff', 'w')
 arfffiletrain.write(header)
 arfffiletest = open('../Corpus/test.arff', 'w')
 arfffiletest.write(header)
 
-print('Reading corpus to arff format')
+print('Reading corpus and finding attributes')
 xmlcorpus = ET.parse('../Corpus/all.xml')
 nodoc = 0
 for doc in xmlcorpus.getroot():
-	# print(nodoc,nodoc%4)
-	text = doc.text
-	# text = text.strip()
-	# text = text.replace("\n", " ")
-	# text = text.replace("'", "\\'")
-	# arfffiletrain.write("'"+text+"'")
-	if 'Améri' in text:
-		monattribut = 1
+	text = doc.text.strip()
+	attrs = ','.join(str(getfeature(text, f)) for f in features)+','+doc.get('class')
+	if nodoc%3:
+		arfffiletrain.write(attrs+'\n')
 	else:
-		monattribut = 0
-	docattr = str(monattribut)+','+doc.get('class')+'\n'
-	if nodoc%3 == 0:
-		arfffiletest.write(docattr)
-	else:
-		arfffiletrain.write(docattr)
+		arfffiletest.write(attrs+'\n')
 	nodoc += 1
