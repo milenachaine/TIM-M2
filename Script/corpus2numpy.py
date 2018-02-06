69fe43b7654396362d84cd3fdb2fa6eabfd37fff
eldams <damien.nouvel@inalco.fr>
2018-02-06 21:39:45 +0100

    Mise à jour des moindres carrés

--- Script/corpus2numpy.py
+++ Script/corpus2numpy.py
@@ -2,20 +2,20 @@
 
 import xml.etree.ElementTree as ET
 import numpy
+numpy.set_printoptions(precision=1,threshold=1000,suppress=True)
 from getfeatures import features, getfeature
 
 print('Reading corpus and finding features')
 xmlcorpus = ET.parse('../Corpus/all.xml')
 nodoc = 0
 
-# nouvelle matrice numpy bonnes dimensions
+print('Création de la matrice numpy pour x et y')
 docs = xmlcorpus.getroot().getchildren()
-featurekeys = [f for f in features]
-print(featurekeys)
+featurekeys = list(features.keys())
 x = numpy.zeros((len(docs), len(featurekeys)))
 y = numpy.zeros((len(docs), 1))
 
-# Remplir la matrice
+print('Insertion des données dans la matrice')
 for i in range(len(docs)):
 	for j in range(len(featurekeys)):
 		doc = docs[i]
@@ -28,20 +28,12 @@ for i in range(len(docs)):
 		y[i,0] = fake
 
 print('Dimensions de la matrice des features: '+str(x.shape))
-
-w = numpy.linalg.lstsq(x,y)[0]
-print(w)
-
-#w = numpy.array([[-0.04589241],[0.00238882],[0.00417351]])
-
-# Calcul x dot w - y puis numpy.linalg.norm
+print('Recherche des poids optimaux par les moindres carrés')
+w = numpy.linalg.lstsq(x,y,rcond=None)[0]
 res = x.dot(w)
 err = y - res
-
-numpy.set_printoptions(precision=1,threshold=1000,suppress=True)
-#print(numpy.hstack((x,y,res, err)))
-
-print(err.sum())
-print(abs(err).sum())
-print('ERR', numpy.linalg.norm(err))
+# print(numpy.hstack((x,y,res, err)))
+# print(err.sum())
+# print(abs(err).sum())
+print('Erreur quadratique: ', numpy.linalg.norm(err))
 
