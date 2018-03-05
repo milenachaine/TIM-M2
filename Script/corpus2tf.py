800add5bc18e71172b2c3595305cc983886d6786
eldams <damien.nouvel@inalco.fr>
2018-03-05 13:26:23 +0100

    Statistiques et tests Tensorflow

--- Script/corpus2tf.py
+++ Script/corpus2tf.py
@@ -32,28 +32,17 @@ print('Dimensions de la matrice des features: '+str(trainX.shape))
 
 numFeatures = trainX.shape[1]
 numLabels = trainY.shape[1]
-numEpochs = 27000
-learningRate = tf.train.exponential_decay(learning_rate=0.0008,
-                                          global_step= 1,
-                                          decay_steps=trainX.shape[0],
-                                          decay_rate= 0.95,
-                                          staircase=True)
+numEpochs = 100
+learningRate = tf.train.exponential_decay(learning_rate=0.0001, global_step= 1, decay_steps=trainX.shape[0], decay_rate= 0.95)
 X = tf.placeholder(tf.float32, [None, numFeatures])
 yGold = tf.placeholder(tf.float32, [None, numLabels])
-weights = tf.Variable(tf.random_normal([numFeatures,numLabels],
-                                       mean=0,
-                                       stddev=(np.sqrt(6/numFeatures+
-                                                         numLabels+1)),
-                                       name="weights"))
-bias = tf.Variable(tf.random_normal([1,numLabels],
-                                    mean=0,
-                                    stddev=(np.sqrt(6/numFeatures+numLabels+1)),
-                                    name="bias"))
+weights = tf.Variable(tf.random_normal([numFeatures,numLabels]))
+bias = tf.Variable(tf.random_normal([1,numLabels]))
 init_OP = tf.initialize_all_variables()
-apply_weights_OP = tf.matmul(X, weights, name="apply_weights")
-add_bias_OP = tf.add(apply_weights_OP, bias, name="add_bias") 
-activation_OP = tf.nn.sigmoid(add_bias_OP, name="activation")
-cost_OP = tf.nn.l2_loss(activation_OP-yGold, name="squared_error_cost")
+apply_weights_OP = tf.matmul(X, weights)
+add_bias_OP = tf.add(apply_weights_OP, bias) 
+activation_OP = tf.nn.sigmoid(add_bias_OP)
+cost_OP = tf.nn.l2_loss(activation_OP-yGold)
 training_OP = tf.train.GradientDescentOptimizer(learningRate).minimize(cost_OP)
 sess = tf.Session()
 sess.run(init_OP)
@@ -74,16 +63,10 @@ for i in range(numEpochs):
     else:
         step = sess.run(training_OP, feed_dict={X: trainX, yGold: trainY})
         if i % 10 == 0:
-            summary_results, train_accuracy, newCost = sess.run(
-                [all_summary_OPS, accuracy_OP, cost_OP], 
-                feed_dict={X: trainX, yGold: trainY}
-            )
+            results, accuracy, newCost = sess.run([all_summary_OPS, accuracy_OP, cost_OP], feed_dict={X: trainX, yGold: trainY})
             diff = abs(newCost - cost)
             cost = newCost
-            print("step %d, training accuracy %g"%(i, train_accuracy))
+            print("step %d, training accuracy %g"%(i, accuracy))
             print("step %d, cost %g"%(i, newCost))
             print("step %d, change in cost %g"%(i, diff))
 print("final accuracy on test set: %s" %str(sess.run(accuracy_OP, feed_dict={X: trainX, yGold: trainY})))
-
-
-
