d52cd052919e54d08af2ec959109d29299dc7979
eldams <damien.nouvel@inalco.fr>
2018-03-05 10:42:53 +0100

    Tests TensorFlow

--- Script/corpus2tf.py
+++ Script/corpus2tf.py
@@ -1,8 +1,8 @@
 #!/bin/env python3
 
 import xml.etree.ElementTree as ET
-import numpy
-numpy.set_printoptions(precision=2,threshold=1000,suppress=True)
+import numpy as np
+np.set_printoptions(precision=2,threshold=1000,suppress=True)
 from getfeatures import features, getfeature
 import matplotlib.pyplot as plt
 import tensorflow as tf
@@ -14,39 +14,76 @@ nodoc = 0
 print('Création de la matrice numpy pour x et y')
 docs = xmlcorpus.getroot().getchildren()
 featurekeys = sorted(list(features.keys()))
-x = numpy.zeros((len(docs), len(featurekeys)))
-y = numpy.zeros((len(docs), 1))
+trainX = np.zeros((len(docs), len(featurekeys)))
+trainY = np.zeros((len(docs), 1))
 
 print('Insertion des données dans la matrice')
 for i in range(len(docs)):
 	for j in range(len(featurekeys)):
 		doc = docs[i]
 		featurename = featurekeys[j]
-		x[i,j] = getfeature(doc, featurename)
+		trainX[i,j] = getfeature(doc, featurename)
 		fake = 0
 		if doc.get('class') == 'fake':
 			fake = 1
-		y[i,0] = fake
+		trainY[i,0] = fake
 
-print('Dimensions de la matrice des features: '+str(x.shape))
+print('Dimensions de la matrice des features: '+str(trainX.shape))
 
-tfx = tf.placeholder(tf.float32, [None, len(featurekeys)])
-tfy_ = tf.placeholder(tf.float32, [None, 1])
-
-tfb = tf.Variable(tf.zeros([1]))
-tfw = tf.Variable(tf.zeros([1,len(featurekeys)]))
-
-tfy = tf.multiply(tfx, tfw) + tfb
-
-loss = tf.reduce_sum((tfy - tfy_) * (tfy - tfy_))
-train_step = tf.train.GradientDescentOptimizer(0.005).minimize(loss)
-
+numFeatures = trainX.shape[1]
+numLabels = trainY.shape[1]
+numEpochs = 27000
+learningRate = tf.train.exponential_decay(learning_rate=0.0008,
+                                          global_step= 1,
+                                          decay_steps=trainX.shape[0],
+                                          decay_rate= 0.95,
+                                          staircase=True)
+X = tf.placeholder(tf.float32, [None, numFeatures])
+yGold = tf.placeholder(tf.float32, [None, numLabels])
+weights = tf.Variable(tf.random_normal([numFeatures,numLabels],
+                                       mean=0,
+                                       stddev=(np.sqrt(6/numFeatures+
+                                                         numLabels+1)),
+                                       name="weights"))
+bias = tf.Variable(tf.random_normal([1,numLabels],
+                                    mean=0,
+                                    stddev=(np.sqrt(6/numFeatures+numLabels+1)),
+                                    name="bias"))
+init_OP = tf.initialize_all_variables()
+apply_weights_OP = tf.matmul(X, weights, name="apply_weights")
+add_bias_OP = tf.add(apply_weights_OP, bias, name="add_bias") 
+activation_OP = tf.nn.sigmoid(add_bias_OP, name="activation")
+cost_OP = tf.nn.l2_loss(activation_OP-yGold, name="squared_error_cost")
+training_OP = tf.train.GradientDescentOptimizer(learningRate).minimize(cost_OP)
 sess = tf.Session()
-sess.run(tf.global_variables_initializer())
+sess.run(init_OP)
+correct_predictions_OP = tf.equal(tf.argmax(activation_OP,1),tf.argmax(yGold,1))
+accuracy_OP = tf.reduce_mean(tf.cast(correct_predictions_OP, "float"))
+activation_summary_OP = tf.summary.histogram("output", activation_OP)
+accuracy_summary_OP = tf.summary.scalar("accuracy", accuracy_OP)
+cost_summary_OP = tf.summary.scalar("cost", cost_OP)
+weightSummary = tf.summary.histogram("weights", weights.eval(session=sess))
+biasSummary = tf.summary.histogram("biases", bias.eval(session=sess))
+all_summary_OPS = tf.summary.merge_all()
+cost = 0
+diff = 1
+for i in range(numEpochs):
+    if i > 1 and diff < .0001:
+        print("change in cost %g; convergence."%diff)
+        break
+    else:
+        step = sess.run(training_OP, feed_dict={X: trainX, yGold: trainY})
+        if i % 10 == 0:
+            summary_results, train_accuracy, newCost = sess.run(
+                [all_summary_OPS, accuracy_OP, cost_OP], 
+                feed_dict={X: trainX, yGold: trainY}
+            )
+            diff = abs(newCost - cost)
+            cost = newCost
+            print("step %d, training accuracy %g"%(i, train_accuracy))
+            print("step %d, cost %g"%(i, newCost))
+            print("step %d, change in cost %g"%(i, diff))
+print("final accuracy on test set: %s" %str(sess.run(accuracy_OP, feed_dict={X: trainX, yGold: trainY})))
 
-for step in range(10):
-	sess.run(train_step, feed_dict={tfx:x,tfy_:y})
-	print(step, sess.run(tfw), sess.run(tfb))
 
-classification = sess.run(tfy, feed_dict={tfx: x})
-print(classification)
+
