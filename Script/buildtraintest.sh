3f120d60dca21af64acb27a9ba0376811db6e091
eldams <damien.nouvel@inalco.fr>
2018-03-26 12:29:30 +0200

    Évaluation par groupes

--- Script/buildtraintest.sh
+++ Script/buildtraintest.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-python3 buildcorpus.py
+echo "Build and split corpus"
+# python3 buildcorpus.py
 python3 corpussplit.py ../Corpus/all.xml
-python3 ../Groupes/lstsq/train.py ../Corpus/all-train.xml model
-python3 ../Groupes/lstsq/predict.py ../Corpus/all-test.xml model predictions.xml
-python3 xmlevaluate.py predictions.xml
-rm model predictions.xml
+
+for g in lstsq scikit-learn; do
+	echo "== TRAIN PREDICT $g =="
+	python3 ../Groupes/$g/train.py ../Corpus/all-train.xml model
+	python3 ../Groupes/$g/predict.py ../Corpus/all-test.xml model predictions.xml
+	python3 xmlevaluate.py predictions.xml
+	rm model predictions.xml
+done
