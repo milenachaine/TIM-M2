a005b927da44951142c54797398e876ae8dda461
eldams <damien.nouvel@inalco.fr>
2018-03-26 11:46:12 +0200

    Scripts de split et d'évaluation

---
+++ Script/buildtraintest.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# python3 buildcorpus.py
+python3 corpussplit.py ../Corpus/all.xml
+python3 ../Groupes/lstsq/train.py ../Corpus/all-train.xml model
+python3 ../Groupes/lstsq/predict.py ../Corpus/all-test.xml model predictions.xml
+python3 xmlevaluate.py predictions.xml
+rm model predictions.xml
