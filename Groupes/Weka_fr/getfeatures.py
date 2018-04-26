2781b7a57ef739e175e8e9c8f070203870fb1de0
Andréa De Sousa <andrea.de.sousa.ads@gmail.com>
2018-04-26 09:46:34 +0200

    Corrections mineures

--- Groupes/Weka_fr/getfeatures.py
+++ Groupes/Weka_fr/getfeatures.py
@@ -7,10 +7,10 @@ def usa_count(text):
     return 1 if re.search(r"\bUSA?\b", text) is not None else 0
 
 def russia_count(text):
-    return 1 if re.search(r"\bRussie\b", text) is not None else 0
-	
+    return 1 if re.search(r"\b[Rr]ussie\b", text) is not None else 0
+
 def korea_count(text):
     return 1 if re.search(r"\b[Cc]orée\b", text) is not None else 0
-	
+
 def disease_count(text):
-    return 1 if re.search(r"\btumeur|cancer\b", text) is not None else 0
+    return 1 if re.search(r"\btumeur|cancer\b", text, flags=re.IGNORECASE) is not None else 0
