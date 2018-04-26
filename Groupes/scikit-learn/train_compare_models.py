02c8f7c2c2de9e93980e555410a9d3f513427aa9
Yousef <abusalha.yousef@hotmail.fr>
2018-04-26 20:58:09 +0200

    Add prediction file

--- Groupes/scikit-learn/train_compare_models.py
+++ Groupes/scikit-learn/train_compare_models.py
@@ -1,6 +1,6 @@
 #!/bin/env python3
 
-import argparse
+import os
 
 import xml.etree.ElementTree as ET
 import numpy as np
@@ -10,20 +10,17 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection  import train_test_split
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, confusion_matrix
 from sklearn.feature_extraction.text import TfidfVectorizer
-
-parser = argparse.ArgumentParser()
-parser.add_argument('xml_path', help='path to the XML file containing the data')
-
-args = parser.parse_args()
-
-xml_path = args.xml_path
+from sklearn.externals import joblib
 
 np.random.seed(42)
 
+if not os.path.isdir('models'):
+  os.mkdir('models')
+
 print('Reading corpus and finding features')
-xmlcorpus = ET.parse(xml_path)
+xmlcorpus = ET.parse('/Users/YOSS/Desktop/classifieur-fakenews/all2.xml')
 nodoc = 0
 
 print('Création de la matrice numpy pour x et y')
@@ -37,6 +34,7 @@ tfidf.fit([d.find('text').text for d in docs])
 
 x_tfidf = tfidf.transform([d.find('text').text for d in docs]).toarray()
 
+joblib.dump(tfidf, os.path.join('models', tfidf.__class__.__name__))
 
 print('Insertion des données dans la matrice')
 for i in range(len(docs)):
@@ -47,6 +45,10 @@ for i in range(len(docs)):
 		fake = 0
 		if doc.get('class') == 'fake':
 			fake = 1
+		# TODO: to go back to binary classification
+		# just comment out the two following lines
+		if doc.get('class') == 'parodic':
+			fake = 2
 		y[i] = fake
 
 x = np.hstack([x, x_tfidf])
@@ -72,14 +74,23 @@ for model in models:
 	yh_train = model.predict(x_train)
 	yh_test = model.predict(x_test)
 
-	print('accuracy_score(train) = {}'.format(accuracy_score(yh_train, y_train)))
-	print('accuracy_score(test) = {}'.format(accuracy_score(yh_test, y_test)))
+	print('accuracy_score(train) = {}'.format(accuracy_score(y_train, yh_train)))
+	print('accuracy_score(test) = {}'.format(accuracy_score(y_test, yh_test)))
 	print('l1(train) = {}'.format(np.linalg.norm(np.abs(yh_train - y_train))))
 	print('l1(test) = {}'.format(np.linalg.norm(np.abs(yh_test - y_test))))
 
+	print('train confusion_matrix:')
+	print(confusion_matrix(y_train, yh_train))
+	print('test confusion_matrix:')
+	print(confusion_matrix(y_test, yh_test))
+
 	print('always zero accuracy (train) = {}'
-		  .format(accuracy_score(np.zeros(yh_train.shape), y_train)))
+		  .format(accuracy_score(y_train, np.zeros(yh_train.shape))))
 	print('always zero accuracy (test) = {}'
-		  .format(accuracy_score(np.zeros(yh_test.shape), y_test)))
-		  
+		  .format(accuracy_score(y_test, np.zeros(yh_test.shape))))
+	
+	path = os.path.join('models', model.__class__.__name__)
+	joblib.dump(model, path)
+	print('model saved in {}'.format(path))
+	
 	print('-' * 20)
