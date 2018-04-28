d9f8dd56093853ab5c94b1d6a225eb1cc336714d
arthur <arthur.provenier@gmail.com>
2018-04-28 21:16:09 +0200

    Prise en compte de Tensorflow, Groupe Tournesol

--- Script/buildtraintest.sh
+++ Script/buildtraintest.sh
@@ -4,10 +4,15 @@ echo "Build and split corpus"
 # python3 buildcorpus.py
 python3 corpussplit.py ../Corpus/all.xml
 
-for g in lstsq scikit-learn; do
+for g in lstsq scikit-learn Tournesol; do
 	echo "== TRAIN PREDICT $g =="
 	python3 ../Groupes/$g/train.py ../Corpus/all-train.xml model
 	python3 ../Groupes/$g/predict.py ../Corpus/all-test.xml model predictions.xml
 	python3 xmlevaluate.py predictions.xml
 	rm model predictions.xml
+    
+    # Pour le modele créé par Tensorflow ... 
+    if [ "$g" == "Tournesol" ]; then
+        rm model* checkpoint
+    fi
 done
