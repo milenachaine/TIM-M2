463d3083ef9ee85b5036a00575b36ef32431b4a5
Chloe <chloe.lecointe@gmail.com>
2018-04-28 11:48:03 +0200

    Ajout de features

--- Groupes/Weka_fr/getfeatures.py
+++ Groupes/Weka_fr/getfeatures.py
@@ -3,6 +3,8 @@
 
 import re
 
+# fake
+
 def usa_count(text):
     return 1 if re.search(r"\bUSA?\b", text) is not None else 0
 
@@ -11,6 +13,46 @@ def russia_count(text):
 
 def korea_count(text):
     return 1 if re.search(r"\b[Cc]orée\b", text) is not None else 0
+	
+def china_count(text):
+    return 1 if re.search(r"\b[Cc]hine\b", text) is not None else 0
+	
+def war_count(text):
+    return 1 if re.search(r"\bwar\b", text, flags=re.IGNORECASE) is not None else 0
+	
+def washington_count(text):
+    return 1 if re.search(r"\bwashington\b", text, flags=re.IGNORECASE) is not None else 0
+	
+# parodic
 
+def ordonnance_count(text):
+    return 1 if re.search(r"\bordonnances?\b", text, flags=re.IGNORECASE) is not None else 0
+	
+def parliament_count(text):
+    return 1 if re.search(r"\bParlement\b", text) is not None else 0
+
+def law_count(text):
+    return 1 if re.search(r"\blois?\b", text, flags=re.IGNORECASE) is not None else 0
+
+def research_count(text):
+    return 1 if re.search(r"\brecherches?\b", text, flags=re.IGNORECASE) is not None else 0
+	
+def ppronounI_count(text):
+    return 1 if re.search(r"\b[Jj]e\b", text) is not None else 0
+	
+# trusted
+ 
 def disease_count(text):
     return 1 if re.search(r"\btumeur|cancer\b", text, flags=re.IGNORECASE) is not None else 0
+	
+def seine_count(text):
+    return 1 if re.search(r"\bseine\b", text, flags=re.IGNORECASE) is not None else 0
+	
+def regression_count(text):
+    return 1 if re.search(r"\br[ée]gression\b", text, flags=re.IGNORECASE) is not None else 0
+	
+def police_count(text):
+    return 1 if re.search(r"\bpolice\b", text, flags=re.IGNORECASE) is not None else 0
+
+def sunday_count(text):
+    return 1 if re.search(r"\bdimanche\b", text, flags=re.IGNORECASE) is not None else 0
