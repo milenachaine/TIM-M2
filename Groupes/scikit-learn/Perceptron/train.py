56654372085014698505c7c52ef1ca76fa1f3617
BELLATO Sandra <bellato.sandra@gmail.com>
2018-04-29 16:02:19 +0200

    modifs perceptron

--- Groupes/scikit-learn/Perceptron/train.py
+++ Groupes/scikit-learn/Perceptron/train.py
@@ -19,22 +19,30 @@ nodoc = 0
 
 print('Création de la matrice numpy pour x et y')
 docs = xmlcorpus.getroot().getchildren()
+
+cmpt_docs = 0
+for i in range(len(docs)) : 
+	doc = docs[i]
+	if doc.get('class') == 'fake' or doc.get('class') == 'trusted':
+		cmpt_docs += 1
+
 featurekeys = sorted(list(features.keys()))
-x = numpy.zeros((len(docs), len(featurekeys)))
-y = numpy.zeros((len(docs)))
+x = numpy.zeros((cmpt_docs, len(featurekeys)))
+y = numpy.zeros((cmpt_docs))
 
 print('Insertion des données dans la matrice')
+cmpt_matrice = 0
 for i in range(len(docs)):
-	for j in range(len(featurekeys)):
-		doc = docs[i]
-		featurename = featurekeys[j]
-		x[i,j] = getfeature(doc, featurename)
-		fake = 0
-		if doc.get('class') == 'fake':
-			fake = 1
-		if doc.get('class') == 'parodic':
-			fake = 2
-		y[i] = fake
+	doc = docs[i]
+	if doc.get('class') != 'parodic':
+		for j in range(len(featurekeys)):
+			featurename = featurekeys[j]
+			x[cmpt_matrice,j] = getfeature(doc, featurename)
+			fake = 0
+			if doc.get('class') == 'fake':
+				fake = 1
+			y[cmpt_matrice] = fake
+		cmpt_matrice +=1
 
 print('Dimensions de la matrice des features: '+str(x.shape))
 
