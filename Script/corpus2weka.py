fd929feb5ff024a9f1ac9ed11f869510c7fdb198
eldams <damien.nouvel@inalco.fr>
2018-12-10 12:30:09 +0100

    Features

--- Script/corpus2weka.py
+++ Script/corpus2weka.py
@@ -5,7 +5,7 @@ import xml.etree.ElementTree as ET
 arff = open('../Corpus/all.arff', 'w')
 
 arff.write('@RELATION juricat\n')
-arff.write('@ATTRIBUTE text STRING\n')
+arff.write('@ATTRIBUTE taille NUMERIC\n')
 arff.write('@ATTRIBUTE cat {assurance,auteur,consommation,divorce,enfants,entreprise,etranger,famille,penal,sante,transport,travail}\n')
 
 arff.write('@DATA\n')
@@ -19,7 +19,7 @@ for doc in corpus:
 			text = doctext.text
 			text = text.replace('\'', '\\\'')
 			text = text.replace('\n', '')
-			arff.write('\''+text+'\'')
+			arff.write(str(len(text)))
 			arff.write(',')
 			arff.write(doc.attrib['class'])
 			arff.write('\n')
