2d15b841772abe0727089a735c4bce52c85aa918
eldams <damien.nouvel@inalco.fr>
2018-12-10 13:41:46 +0100

    Regénération corpus

--- Script/corpus2weka.py
+++ Script/corpus2weka.py
@@ -2,11 +2,14 @@
 
 import xml.etree.ElementTree as ET
 
+# /Users/damien/Desktop
+
 arff = open('../Corpus/all.arff', 'w')
 
 arff.write('@RELATION juricat\n')
 arff.write('@ATTRIBUTE taille NUMERIC\n')
 arff.write('@ATTRIBUTE travail NUMERIC\n')
+arff.write('@ATTRIBUTE entreprise NUMERIC\n')
 arff.write('@ATTRIBUTE cat {assurance,consommation,entreprise,etranger,famille,penal,travail}\n')
 
 arff.write('@DATA\n')
@@ -22,10 +25,18 @@ for doc in corpus:
 			text = text.replace('\n', '')
 			arff.write(str(len(text)))
 			arff.write(',')
-			if 'travail' in text:
+			if 'travail' in text or 'salaire' in text or 'salarié' in text or 'Smic' in text:
+				arff.write('1')
+			else:
+				arff.write('0')
+			arff.write(',')
+			if 'entreprise' in text:
 				arff.write('1')
 			else:
 				arff.write('0')
 			arff.write(',')
 			arff.write(doc.attrib['class'])
 			arff.write('\n')
+
+
+
