ecd0d2b048f3472f3fb447e9cf257a560b4722ba
eldams <damien.nouvel@inalco.fr>
2019-02-18 13:20:20 +0100

    Classif scikit avec filtrage et split

--- Script/scikit.py
+++ Script/scikit.py
@@ -1,31 +1,35 @@
 #!/bin/env python3
 
 import xml.etree.ElementTree as ET
-
 tree = ET.parse('../Groupes/Crawling/corpusIrisVersion3.xml')
 corpus = tree.getroot()
-
-import numpy, random
-
-nbdata = len(corpus)
-print('Il y a', nbdata, 'données')
 docs = []
 for doc in corpus:
 	docs.append(doc)
+nbdata = len(docs)
+print('Il y a', nbdata, 'données')
+
+import numpy, random
 random.shuffle(docs)
 tgtclass = 'Immobilier'
-ydata = numpy.zeros((nbdata, 1))
+ydata = []
 xdatatext = []
 datacount = 0
 for doc in docs:
-	if doc.attrib['class'] == tgtclass:
-		ydata[datacount, 0] = 1
 	questions = ''
 	for child in doc:
 		if child.tag == 'question':
-			questions += child.text
-	xdatatext.append(questions)
-	datacount += 1
+			questions += '\n'+child.text.strip()
+	if len(questions) > 10:
+		if doc.attrib['class'] == tgtclass:
+			ydata.append(1)
+		else:
+			ydata.append(0)
+		xdatatext.append(questions)
+		datacount += 1
+nbdata = datacount
+print('Après filtrage il reste ', nbdata, 'données')
+ydata = numpy.asarray(ydata)
 print('Il y a', ydata.sum(), 'données de la classe', tgtclass)
 
 import sklearn
@@ -37,8 +41,8 @@ print('La vectorisation donne', nbfeatures, 'features')
 
 from sklearn.model_selection import train_test_split
 xdata, xdatatest, ydata, ydatatest = train_test_split(xdata, ydata, test_size=0.3, random_state=0)
-nbdatatest = xdatatest.shape[1]
-print('Le split écarte', nbdatatest, 'données')
+nbdatatest = xdatatest.shape[0]
+print('Le split écarte', nbdatatest, 'données pour le test')
 
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.tree import DecisionTreeClassifier
@@ -47,16 +51,5 @@ model.fit(xdata, ydata)
 ypredtest = model.predict(xdatatest)
 ypredtest = ypredtest.reshape(ydatatest.shape)
 ypredtest = numpy.rint(ypredtest)
-erreur = ydatatest - ypredtest
-nberreurs = abs(erreur).sum()
-print('Erreur,', nberreurs/nbdatatest)
-
-from sklearn.linear_model import LogisticRegression
-model = LogisticRegression(random_state=0, solver='lbfgs')
-model.fit(xdata, ydata)
-ypredtest = model.predict(xdatatest)
-ypredtest = ypredtest.reshape(ydatatest.shape)
-ypredtest = numpy.rint(ypredtest)
-erreur = ydatatest - ypredtest
-nberreurs = abs(erreur).sum()
-print('Erreur,', nberreurs/nbdatatest)
+nberreurs = abs(ydatatest - ypredtest).sum()
+print('Erreur de classification', nberreurs/nbdatatest)
