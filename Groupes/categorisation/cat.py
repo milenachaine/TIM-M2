9e56ed33113d1aa18c91fdd8a7862a97bd6fe0a1
yizhou <yizhou.xu8859@gmail.com>
2019-02-25 11:10:06 +0100

    mod cat.py

--- Groupes/categorisation/cat.py
+++ Groupes/categorisation/cat.py
@@ -14,9 +14,8 @@ from sklearn.externals import joblib
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
-from sklearn.model_selection import GridSearchCV
 
-FEATURE_SIZE = 500
+FEATURE_SIZE = 1000
 DOC_CLASS = {
     "immobilier": "imm",
     "travail": "trv",
@@ -67,43 +66,6 @@ def main():
 
     # save pipeline
     joblib.dump(tfidf_rf_clf, "tfidf_rf_clf")
-
-    # create arff
-    # features = vectorizer.get_feature_names()
-    # sets = {
-    #     'x_train': x_train,
-    #     'x_test': x_test,
-    #     'y_train': y_train,
-    #     'y_test': y_test
-    # }
-    # corpus2arff(features,**sets)
-
-def corpus2arff(features,x_train,x_test,y_train,y_test):
-    with open("train.arff", "w", encoding="utf-8") as TRAIN, \
-            open("test.arff","w", encoding="utf-8") as TEST:
-        TRAIN.write("@RELATION juritique\n")
-        for f in features:
-            TRAIN.write("@ATTRIBUTE "+f+"\tNUMERIC\n")
-        TRAIN.write(
-            "@ATTRIBUTE class\t{{{0}}}\n".format(", ".join(DOC_CLASS.values()))
-        )
-        TRAIN.write("@DATA\n")
-        for i in range(x_train.shape[0]):
-            TRAIN.write(", ".join(str(j) for j in x_train[i]))
-            TRAIN.write(", "+y_train[i]+"\n")
-
-        TEST.write("@RELATION juritique\n")
-        for f in features:
-            TEST.write("@ATTRIBUTE " + f + "\tNUMERIC\n")
-        TEST.write(
-            "@ATTRIBUTE class\t{{{0}}}\n".format(", ".join(DOC_CLASS.values()))
-        )
-        TEST.write("@DATA\n")
-        for i in range(x_test.shape[0]):
-            TEST.write(", ".join(str(j) for j in x_test[i]))
-            TEST.write(", "+y_test[i]+"\n")
-
-
 
 if __name__ == "__main__":
     main()
