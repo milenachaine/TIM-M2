67cb270f94dccf7ba7ab89d20036bc51fa117cd0
DongChuanming <dongchuanming@yahoo.com>
2019-03-20 12:17:47 +0100

    accelerate

--- Groupes/Pretraitement/xml2conll.py
+++ Groupes/Pretraitement/xml2conll.py
@@ -1,4 +1,5 @@
-
+from treetaggerwrapper import TreeTagger, make_tags
+TAGGER = TreeTagger(TAGLANG='fr',TAGOPT="-token -lemma -sgml -no-unknown") 
 import sys
 import os
 import xml.etree.ElementTree as ET
@@ -18,18 +19,26 @@ for i in corpus.findall("./doc"):
     ID_q=1
     ID_a=1 
     for q in i.findall("./question"):
-        #with open(mydir+"/"+source+"/"+cat+"/"+sub+"/"+ID+"/"+ID+"_q"+str(ID_q)+".conll", "w", encoding="utf-8") as question:
-        for line in q.text.split("\n"):
-            os.system('echo "'+line.strip().replace('"','\"')+'" | tree-tagger-french >>'+mydir+"/"+source+"/"+cat+"/"+sub+"/"+ID+"/"+ID+"_q"+str(ID_q)+".conll")
+        with open(mydir+"/"+source+"/"+cat+"/"+sub+"/"+ID+"/"+ID+"_q"+str(ID_q)+".conll", "w", encoding="utf-8") as question:
+            for line in q.text.split("\n"):
+            #os.system('echo "'+line.strip().replace('"','\"')+'" | tree-tagger-french >>'+mydir+"/"+source+"/"+cat+"/"+sub+"/"+ID+"/"+ID+"_q"+str(ID_q)+".conll")
+                tags = TAGGER.tag_text(line)
+                for tag in tags:
+                    question.write(tag+"\n")
+                question.write("\n")
+                
         #print(q.text.strip().replace('"','\"'))
         phrases.write(q.text.strip())
         ID_q+=1
     for a in i.findall("./answer"):
-        #with open(mydir+"/"+source+"/"+cat+"/"+sub+"/"+ID+"/"+ID+"_a"+str(ID_a)+".conll", "w", encoding="utf-8") as answer:
-        for line in a.text.split("\n"):
-            os.system('echo "'+line.strip().replace('"','\"')+'" | tree-tagger-french >>'+mydir+"/"+source+"/"+cat+"/"+sub+"/"+ID+"/"+ID+"_a"+str(ID_a)+".conll")
-        #print(q.text.strip().replace('"','\"'))
-        phrases.write(a.text.strip())
+        with open(mydir+"/"+source+"/"+cat+"/"+sub+"/"+ID+"/"+ID+"_a"+str(ID_q)+".conll", "w", encoding="utf-8") as answer:
+            for line in a.text.split("\n"):
+            #os.system('echo "'+line.strip().replace('"','\"')+'" | tree-tagger-french >>'+mydir+"/"+source+"/"+cat+"/"+sub+"/"+ID+"/"+ID+"_q"+str(ID_q)+".conll")
+                tags = TAGGER.tag_text(line)
+                for tag in tags:
+                    answer.write(tag+"\n")
+                answer.write("\n")
+        phrases.write(q.text.strip())
         ID_a+=1
 
 #os.system("bash parcours.sh "+mydir+"/"+source)
