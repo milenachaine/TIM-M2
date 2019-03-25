4a6bc92b8214330185b760836d52e937bd4f8cc0
DongChuanming <dongchuanming@yahoo.com>
2019-03-25 12:30:35 +0100

    new

--- Groupes/Pretraitement/phrase2conll.py
+++ Groupes/Pretraitement/phrase2conll.py
@@ -1,8 +1,15 @@
-
+from treetaggerwrapper import TreeTagger, make_tags
+TAGGER = TreeTagger(TAGLANG='fr',TAGOPT="-token -lemma -sgml -no-unknown") 
 import sys
 import os
-phrase = str(sys.argv[1])
-os.system('echo "'+phrase.replace('"','\"')+'" | tree-tagger-french > phrase.conll')
-
+def main():
+    phrase=sys.argv[1]
+    line=""
+    tags = TAGGER.tag_text(phrase)
+    for tag in tags:
+        line+=tag.replace("\t","/")+" "
+    print("tagged phrase : "+line)
+    return line
+if __name__ == "__main__":
+    main()
     
- 
