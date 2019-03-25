f6ee5b5a8372c2010e47e3791a5bef2f2307ff5d
eldams <damien.nouvel@inalco.fr>
2019-03-25 12:36:51 +0100

    Script test de similarité

--- Groupes/interaction/flask-ChatLaw/test_question.py
+++ Groupes/interaction/flask-ChatLaw/test_question.py
@@ -1,34 +1,38 @@
 import pickle
 
-relpath = '../Similarite/'
-documents_Corpus = pickle.load(open(relpath+"documents_Corpus.pkl", "rb"))
-vectorizer = pickle.load(open(relpath+"vectorizer.pkl", "rb"))
-TfIdfQuestions = pickle.load(open(relpath+"TfIdfQuestions.pkl", "rb"))
-liste_questions = pickle.load(open(relpath+"liste_questions.pkl", "rb"))
+path = '/home/teamlaw/git-TIM-M2/Groupes/Similarite/'
+# path = '../../Similarite/'
+documents_Corpus = pickle.load(open(path+"documents_Corpus.pkl", "rb"))
+vectorizer = pickle.load(open(path+"vectorizer.pkl", "rb"))
+TfIdfQuestions = pickle.load(open(path+"TfIdfQuestions.pkl", "rb"))
+liste_questions = pickle.load(open(path+"liste_questions.pkl", "rb"))
 
-question_utilisateur = "J'ai des factures impayées"
-TfIdfQuestions_utilisateur = vectorizer.transform([question_utilisateur], True)
+def getBestQuestion(question_utilisateur):
+    TfIdfQuestions_utilisateur = vectorizer.transform([question_utilisateur], True)
 
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn import metrics, feature_extraction
+    from sklearn.feature_extraction.text import TfidfVectorizer
+    from sklearn import metrics, feature_extraction
 
-print("Question utilisateur = ",question_utilisateur)
-bestQuestionScore = None
-bestQuestionIndex = None
-for questionIndex in range (len(liste_questions)):
-    #print("    Question: ",liste_questions[questionIndex])
-    # Ici on vérifie que la classe du document est bien celle qu'on veut
-    index_doc = "iris" + str(questionIndex + 1)
-    if documents_Corpus.get(index_doc)['class'] == "Entreprise":
+    # print("Question utilisateur = ",question_utilisateur)
+    bestQuestionScore = None
+    bestQuestionIndex = None
+    for questionIndex in range (len(liste_questions)):
+        #print("    Question: ",liste_questions[questionIndex])
+        # Ici on vérifie que la classe du document est bien celle qu'on veut
+        index_doc = "iris" + str(questionIndex + 1)
+        if documents_Corpus.get(index_doc)['class'] == "Entreprise":
 
-        simScore = metrics.pairwise.cosine_similarity(TfIdfQuestions[questionIndex], TfIdfQuestions_utilisateur[0])
-        #print ("        simScore: ",simScore)
-        if not bestQuestionScore or simScore > bestQuestionScore:
-            bestQuestionScore = simScore
-            bestQuestionIndex = questionIndex
+            simScore = metrics.pairwise.cosine_similarity(TfIdfQuestions[questionIndex], TfIdfQuestions_utilisateur[0])
+            #print ("        simScore: ",simScore)
+            if not bestQuestionScore or simScore > bestQuestionScore:
+                bestQuestionScore = simScore
+                bestQuestionIndex = questionIndex
 
-print("  ===> Best Question: ")
-if (bestQuestionIndex):
-    index = "iris" + str(bestQuestionIndex)
-    print("       QuestionIndex: ",bestQuestionIndex," => ", documents_Corpus.get(index))
-    
+    # print("  ===> Best Question: ")
+    if (bestQuestionIndex):
+        index = "iris" + str(bestQuestionIndex)
+        return documents_Corpus.get(index)
+    else:
+        return None
+        
+print(getBestQuestion("J'ai des factures impayées"))
